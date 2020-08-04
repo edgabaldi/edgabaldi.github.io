73a478998a7d752a1262b661030bea7edbeec44b
Edgar <edgabaldi@gmail.com>
2020-08-04 15:21:03 -0400

    PWAzation.

--- investing/js/popup.js
+++ investing/js/popup.js
@@ -1,17 +1,19 @@
 
-const strWindowFeatures = "width=450,height=450,menubar=no,location=no,resizable=no,scrollbars=yes,status=no";
+const strIndicadoresWindowProps = "width=450,height=450,menubar=no,location=no,resizable=no,scrollbars=yes,status=no";
+const strCalendarWindowProps = "width=640,height=400,menubar=no,location=no,resizable=no,scrollbars=yes,status=no";
+
 let windowObjectReference;
 
 function openPopupIndicadores(){
     windowObjectReference = window.open(
         'investing/index.html', 
         'Monitor',
-        strWindowFeatures);
+        strIndicadoresWindowProps);
 }
 
 function openPopupCalendario(){
     windowObjectReference = window.open(
         'investing/calendar.html', 
         'Calendário',
-        strWindowFeatures);
+        strCalendarWindowProps);
 }
