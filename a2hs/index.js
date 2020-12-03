9812ada4c81fd2274db6a08728947fbb998ab32f
Edgar <edgabaldi@gmail.com>
2020-12-03 15:59:22 -0400

    fix paths

--- a2hs/index.js
+++ a2hs/index.js
@@ -15,7 +15,7 @@ setInterval(function() {
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/pwa-examples/a2hs/sw.js')
+           .register('/a2hs/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
--- a2hs/sw.js
+++ a2hs/sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('fox-store').then(function(cache) {
      return cache.addAll([
-       '/pwa-examples/a2hs/',
-       '/pwa-examples/a2hs/index.html',
-       '/pwa-examples/a2hs/index.js',
-       '/pwa-examples/a2hs/style.css',
-       '/pwa-examples/a2hs/images/fox1.jpg',
-       '/pwa-examples/a2hs/images/fox2.jpg',
-       '/pwa-examples/a2hs/images/fox3.jpg',
-       '/pwa-examples/a2hs/images/fox4.jpg'
+       '/a2hs/',
+       '/a2hs/index.html',
+       '/a2hs/index.js',
+       '/a2hs/style.css',
+       '/a2hs/images/fox1.jpg',
+       '/a2hs/images/fox2.jpg',
+       '/a2hs/images/fox3.jpg',
+       '/a2hs/images/fox4.jpg'
      ]);
    })
  );
